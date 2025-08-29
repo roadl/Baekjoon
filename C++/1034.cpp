499ecf7ca19509e370ae4726624ca5a2394207e1
yojin <roadlroal@naver.com>
2025-08-29 20:51:38 +0900

    8/29 lamp solve

--- C++/1034.cpp
+++ C++/1034.cpp
@@ -4,9 +4,50 @@
 
 using namespace std;
 
+int check(vector<string> vec, string s, int n)
+{
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (vec[i].compare(s) == 0)
+			count++;
+	}
+
+	return count;
+}
+
 void solve(vector<string> vec, int n, int m, int k)
 {
-	
+	vector<int> switch_on(m);
+
+	int count, cur, max = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::fill(switch_on.begin(), switch_on.end(), 0);
+		count = 0;
+
+		for (int j = 0; j < m; j++)
+		{
+			if (vec[i][j] == '0')
+			{
+				switch_on[j] = 1;
+				count++;
+			}
+		}
+
+		if (count <= k && (k - count) % 2 == 0)
+		{
+			cur = check(vec, vec[i], n);
+			if (cur > max)
+				max = cur;
+		}
+
+		//cout << i << ": " << count << " - " << cur << endl;
+	}
+
+	cout << max << endl;
 }
 
 int main()
@@ -28,12 +69,12 @@ int main()
 
 	cin >> k;
 
-	for (string s: vec)
-	{
-		cout << s << endl;
-	}
+	// for (string s: vec)
+	// {
+	// 	cout << s << endl;
+	// }
 
-	cout << k << endl;
+	// cout << k << endl;
 
 	solve(vec, n, m, k);
 

8478d1c7651a9cca9803c5d256d69a164af034e9
yojin <roadlroal@naver.com>
2026-01-12 12:25:52 +0900

    1074 refactor

--- C++/1074.cpp
+++ C++/1074.cpp
@@ -5,28 +5,32 @@
 using namespace std;
 
 // 1. 방문하면서 ++시켜서 확인
-// 비 효율적임 코테면 이렇게 풀수도? 2^15면 1024 * 32 = 3만언저리 시간 통과 가능
+// 2^15면 1024 * 32 = 3만언저리 시간 통과 가능
 // 2. 규칙 찾아서 계산
 // 칸 방문 순서는 정해져잇음
 // 칸을 1/4 씩 나눠서 확인하기
 
 void solve(int N, int r, int c)
 {
-	int t = pow(2, N - 1);
+	int half;
 	int ans = 0;
 
 	while (N > 0)
 	{
-		if (c >= pow(2, N - 1))
+		half = pow(2, N - 1);
+
+		if (c >= half)
 		{
-			c -= pow(2, N - 1);
+			c -= half;
 			ans += 1 * pow(4, N - 1);
 		}
-		if (r >= pow(2, N - 1))
+
+		if (r >= half)
 		{
-			r -= pow(2, N - 1);
+			r -= half;
 			ans += 2 * pow(4, N - 1);
 		}
+		
 		N--;
 	}
 

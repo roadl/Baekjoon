fc7c0832c9222dd61cd59f14730c94940962b320
yojin <roadlroal@naver.com>
2026-02-06 13:42:40 +0900

    26.2.6 1103 timeout

---
+++ C++/1103.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int N, M;
+
+vector<string> map;
+bool visited[51][51] = {0};
+int res = 0;
+
+void recur(int x, int y, int cnt) {
+	if (y < 0 || y >= N || x < 0 || x >= M ||
+		map[y][x] == 'H') {
+		if (res < cnt)
+			res = cnt;
+		return;
+	}
+
+	if (visited[y][x]) {
+		cout << "-1\n";
+		exit(0);
+	}
+
+	dp[y][x] = cnt;
+
+	visited[y][x] = true;
+
+	int offset = map[y][x] - '0';
+
+	recur(x + offset, y, cnt + 1);
+	recur(x - offset, y, cnt + 1);
+	recur(x, y + offset, cnt + 1);
+	recur(x, y - offset, cnt + 1);
+
+	visited[y][x] = false;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		map.push_back(str);
+	}
+
+	recur(0, 0, 0);
+
+	cout << res << "\n";
+}

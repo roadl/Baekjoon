1fe8a6825547d9be77140e986194631548e4881f
roadl <roadlroal@naver.com>
2026-03-12 15:45:57 +0900

    26.3.12 1644 solve

---
+++ C++/1644.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int N;
+vector<int> primes;
+map<int, int> m;
+
+void get_prime(int n) {
+	vector<bool> is_prime(n + 1, true);
+
+	is_prime[0] = false;
+	is_prime[1] = false;
+
+	for (int i = 2; i * i <= n; i++)
+		if (is_prime[i])
+			for (int j = i * i; j <= n; j += i)
+				is_prime[j] = false;
+
+	for (int i = 2; i <= n; i++)
+		if (is_prime[i])
+			primes.push_back(i);
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+
+	get_prime(N);
+
+	int left = 0, right = 0;
+	int sum = 0;
+	int res = 0;
+
+	while (true) {
+		if (sum == N)
+			res++;
+		if (sum >= N) {
+			sum -= primes[left];
+			left++;
+		} else {
+			if (right == primes.size())
+				break;
+			sum += primes[right];
+			right++;
+		}
+	}
+
+	cout << res << endl;
+
+	return 0;
+}

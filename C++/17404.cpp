d3d9f4ee2eab9c50d96ad23170051f088e0fbf72
roadl <roadlroal@naver.com>
2026-03-12 11:00:21 +0900

    26.3.12 17404 refactor

--- C++/17404.cpp
+++ C++/17404.cpp
@@ -6,9 +6,7 @@
 using namespace std;
 
 int N;
-unsigned int dp[1000][3][3];
-int min_res;
-int res;
+int dp[1000][3][3];
 vector<int> R, G, B;
 
 int main()
@@ -24,7 +22,7 @@ int main()
 
 	for (int i = 0; i < N; i++)
 		cin >> R[i] >> G[i] >> B[i];
-	
+
 	// 첫번째 집 R
 	dp[0][0][0] = R[0];
 	dp[0][1][0] = 1001;
@@ -34,33 +32,30 @@ int main()
 	dp[0][0][1] = 1001;
 	dp[0][1][1] = G[0];
 	dp[0][2][1] = 1001;
-	
+
 	// 첫번째 집 B
 	dp[0][0][2] = 1001;
 	dp[0][1][2] = 1001;
 	dp[0][2][2] = B[0];
 
-	for (int i = 1; i < N; i++) {
-		dp[i][0][0] = min(dp[i - 1][1][0] + R[i], dp[i - 1][2][0] + R[i]);
-		dp[i][1][0] = min(dp[i - 1][0][0] + G[i], dp[i - 1][2][0] + G[i]);
-		dp[i][2][0] = min(dp[i - 1][0][0] + B[i], dp[i - 1][1][0] + B[i]);
-
-		dp[i][0][1] = min(dp[i - 1][1][1] + R[i], dp[i - 1][2][1] + R[i]);
-		dp[i][1][1] = min(dp[i - 1][0][1] + G[i], dp[i - 1][2][1] + G[i]);
-		dp[i][2][1] = min(dp[i - 1][0][1] + B[i], dp[i - 1][1][1] + B[i]);
-		
-		dp[i][0][2] = min(dp[i - 1][1][2] + R[i], dp[i - 1][2][2] + R[i]);
-		dp[i][1][2] = min(dp[i - 1][0][2] + G[i], dp[i - 1][2][2] + G[i]);
-		dp[i][2][2] = min(dp[i - 1][0][2] + B[i], dp[i - 1][1][2] + B[i]);
+	for (int start = 0; start < 3; start++) {
+		for (int i = 1; i < N; i++) {
+			dp[i][0][start] = min(dp[i - 1][1][start] + R[i], dp[i - 1][2][start] + R[i]);
+			dp[i][1][start] = min(dp[i - 1][0][start] + G[i], dp[i - 1][2][start] + G[i]);
+			dp[i][2][start] = min(dp[i - 1][0][start] + B[i], dp[i - 1][1][start] + B[i]);
+		}
 	}
 
-	res = min(dp[N - 1][1][0], dp[N - 1][2][0]);
-	min_res = min(dp[N - 1][0][1], dp[N - 1][2][1]);
-	res = min(res, min_res);
-	min_res = min(dp[N - 1][0][2], dp[N - 1][1][2]);
-	res = min(res, min_res);
+	int ans = 1e9;
 
-	cout << res << endl;
+	for (int start = 0; start < 3; start++) {
+		for (int end = 0; end < 3; end++) {
+			if (start == end) continue;
+			ans = min(ans, dp[N - 1][end][start]);
+		}
+	}
+
+	cout << ans << endl;
 
 	return 0;
 }
